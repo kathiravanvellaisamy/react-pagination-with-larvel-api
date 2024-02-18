772bb89109883b86622d1790717df7db9ec925db
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2024-02-18 08:33:06 +0530

    Show all the products from db

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -16,8 +16,9 @@ class ProductController extends Controller
     public function index()
     {
 
+        $products = Product::all();
         return response()->json([
-            'results' => "all the records"
+            'results' => $products
         ],200);
     }
 
