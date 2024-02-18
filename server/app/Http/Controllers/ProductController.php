1947723a68ae845902ef457aabd68ea60e0dcaee
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2024-02-18 08:24:09 +0530

    Created Store function

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreProductRequest;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
@@ -29,9 +32,30 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
-        //
+        try {
+             $imageName = Str::random(32).".".$request->image->getClientOriginalExtension();
+
+             Product::create([
+                'name' => $request->name,
+                'description' => $request->description,
+                'price' => $request->price,
+                'image' => $imageName,
+             ]);
+
+             Storage::disk('public')->put($imageName,file_get_contents($request->image));
+
+             return response()->json([
+                'message' => "Product created successfully..."
+             ],200);
+
+        } catch (\Exception $e) {
+
+            return response()->json([
+                'message' => "Something went wrong"
+            ],500);
+        }
     }
 
     /**
