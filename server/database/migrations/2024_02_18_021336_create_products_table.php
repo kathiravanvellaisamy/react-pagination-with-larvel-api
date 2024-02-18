503116aee86dbf4ed09506bb4981a1b6c2e79526
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2024-02-18 08:00:22 +0530

    created columns

--- server/database/migrations/2024_02_18_021336_create_products_table.php
+++ server/database/migrations/2024_02_18_021336_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price', 9, 3);
+            $table->longText('description');
+            $table->string('image');
             $table->timestamps();
         });
     }
